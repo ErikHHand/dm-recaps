fa16ab34576f1e3da12149ca0da173db7367801e
Erik Handberg <erikhand@kth.se>
2019-06-22 20:49:35 +0200

    Basic layout for sign up file

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-const SignUp = () => (
-    <div></div>
-  );
-  
-export default SignUp;
+import * as ROUTES from '../../constants/routes';
+
+const SignUpPage = () => (
+    <div>
+      	<h1>SignUp</h1>
+    	<SignUpForm />
+  	</div>
+);
+
+class SignUpForm extends Component {
+  	constructor(props) {
+    	super(props);
+  	}
+
+	onSubmit = event => {
+
+	}
+
+	onChange = event => {
+
+	};
+
+	render() {
+		return (
+			<form onSubmit={this.onSubmit}>
+
+			</form>
+		);
+	}
+}
+
+const SignUpLink = () => (
+	<p>
+		Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+	</p>
+);
+
+export default SignUpPage;
+
+export { SignUpForm, SignUpLink };
