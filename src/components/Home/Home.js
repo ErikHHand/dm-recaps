9fa3e5b0fe2047d57c3e00ce454695ddfdb5db4f
Erik Handberg <erikhand@kth.se>
2019-07-03 14:57:39 +0200

    Basic protected routes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { withAuthorization } from '../Session/Session';
 
-const Home = () => (
-    <div></div>
-  );
-  
-export default Home;
+const HomePage = () => (
+    <div>
+      	<h1>Home Page</h1>
+      	<p>The Home Page is accessible by every signed in user.</p>
+    </div>
+);
+
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(HomePage);
