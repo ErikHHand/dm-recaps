2717540f15eec491f9bf2c3c882fe98296fb3455
Erik Handberg <erikhand@kth.se>
2020-03-07 15:51:17 +0100

    Comments and fixes to tagsPage component

--- src/components/TagsPage/TagsPage.js
+++ src/components/TagsPage/TagsPage.js
@@ -7,12 +7,13 @@ import TagInfo from '../TagInfo/TagInfo';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button';
-//import Form from 'react-bootstrap/Form'
-import Badge from 'react-bootstrap/Badge'
 
 import { withFirebase } from '../Firebase/Firebase';
 import * as firebase from 'firebase'; // Ta inte bort
 
+/*
+	This component holds the tags tab of the App.
+*/
 class TagsPage extends Component {
 
 	constructor(props) {
@@ -23,9 +24,12 @@ class TagsPage extends Component {
 			currentTag: null,
 		};
 
+		// Set the context for "this" for the following function
 		this.handleCurrentTag = this.handleCurrentTag.bind(this);
 	}
 
+	// Handles changing which tag is the current tag,
+	// which tag is currently selected
 	handleCurrentTag(tagID) {
 		this.setState({
 			currentTag: tagID,
@@ -34,55 +38,15 @@ class TagsPage extends Component {
 
 	render() {
 
-		let recapItems;
-
-		if(!this.state.currentTag) {
-			recapItems = <div></div>;
-		} else if(!this.props.tags[this.state.currentTag]) {
-			recapItems = <div></div>;	 
-		} else {
-			let recapList = this.props.tags[this.state.currentTag].recaps;
-
-			// Order recap items chronologically
-
-			let length = this.props.campaign.sessionOrder.length;
-			let recapKeys = {};
-
-			for(let recapItem in recapList) {
-				let session = this.props.campaign.sessions[recapList[recapItem].session];
-				let sessionIndex = this.props.campaign.sessionOrder.indexOf(recapList[recapItem].session);
-				recapKeys[recapItem] = (length - sessionIndex) * 100000 + session.recapOrder.indexOf(Number(recapItem));
-			}			
-
-			// Sort keys
-			let sortedKeys = Object.keys(recapKeys).sort((a, b) => {				
-				return recapKeys[a] - recapKeys[b];
-			});
-
-			recapItems = sortedKeys.map((recapID)=>
-				<RecapItem 
-					key = {recapID}
-					recapID = {recapID}
-					recapItem = {recapList[recapID]}
-					tags = {this.props.tags}
-					sessions = {this.props.sessions}
-					handleSessions = {this.props.handleSessions}
-					handleTags = {this.props.handleTags}
-					handleCampaign = {this.props.handleCampaign}
-					id = {this.props.campaignID}
-					campaign = {this.props.campaign}
-				/>
-			);
-		}
-
-		let tagItems;
-
 		let tagsPage = this;
 
+		// Render tag items
+		let tagItems;
+
 		if(!this.props.campaign.tags) {
-			tagItems = <div></div>;
+			tagItems = <div></div>; //Render nothing if there are no tags
 		} else {
-			// Sort keys in date order
+			// Sort keys in date order, meaning tags created more recently will appear higher up
 			let sortedKeys = Object.keys(this.props.campaign.tags).sort((a, b) => {				
 				return this.props.campaign.tags[b].created.toDate() - this.props.campaign.tags[a].created.toDate();
 			});
@@ -101,6 +65,50 @@ class TagsPage extends Component {
 					handleCurrentTag = {this.handleCurrentTag}
 					handleClick = {() => tagsPage.setState({currentTag: tag})}
 					id = {this.props.campaignID}
+				/>
+			);
+		}
+
+		// Render recap items
+		let recapItems;
+
+		if(!this.state.currentTag) {
+			recapItems = <div></div>; // No current tag
+		} else if(!this.props.tags[this.state.currentTag]) {
+			recapItems = <div></div>; // // Current tag doesn't exist?
+		} else {
+
+			let recapList = this.props.tags[this.state.currentTag].recaps;
+			let length = this.props.campaign.sessionOrder.length;
+			let recapKeys = {};
+
+			// Order recap items chronologically
+			// First, give each recap item a number for sorting
+			// Multiply session order by a large number to guarantee that is counted higher
+			// than the recap order, which is added by as a small number
+			for(let recapItem in recapList) {
+				let session = this.props.campaign.sessions[recapList[recapItem].session];
+				let sessionIndex = this.props.campaign.sessionOrder.indexOf(recapList[recapItem].session);
+				recapKeys[recapItem] = (length - sessionIndex) * 100000 + session.recapOrder.indexOf(Number(recapItem));
+			}			
+
+			// Then sort keys
+			let sortedKeys = Object.keys(recapKeys).sort((a, b) => {				
+				return recapKeys[a] - recapKeys[b];
+			});
+
+			recapItems = sortedKeys.map((recapID)=>
+				<RecapItem 
+					key = {recapID}
+					recapID = {recapID}
+					recapItem = {recapList[recapID]}
+					tags = {this.props.tags}
+					sessions = {this.props.sessions}
+					handleSessions = {this.props.handleSessions}
+					handleTags = {this.props.handleTags}
+					handleCampaign = {this.props.handleCampaign}
+					id = {this.props.campaignID}
+					campaign = {this.props.campaign}
 				/>
 			);
 		}
