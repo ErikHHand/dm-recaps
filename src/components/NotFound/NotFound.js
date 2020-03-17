3fc0711f2e1f9e96ea226e2ce6c427e0fd3205dc
Erik Handberg <erikhand@kth.se>
2020-03-17 21:20:13 +0100

    Comment change to NotFound component

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Container, Button } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
-// Component that is render in case of 404 Not Found
+// Component that is rendered in case of 404 Not Found
 class NotFound extends Component {
     render() {
 
