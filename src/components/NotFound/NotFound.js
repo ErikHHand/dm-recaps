8060a46c659ea3e0f4f7f3e0dfa18cb69a7a2c46
Erik Handberg <erikhand@kth.se>
2020-03-18 23:10:46 +0100

    Cleanup

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -5,21 +5,21 @@ import { Link } from "react-router-dom";
 // Component that is rendered in case of 404 Not Found
 class NotFound extends Component {
     render() {
-
-        return  <Container className="text-white">
-                    <p className="h1">
-                        404.. This page is not found!
-                    </p>
-                    <p>
-                        So sorry about that!
-                    </p>
-                    <Link to="/">
-                        <Button>
-                            Return to homepage
-                        </Button>    
-                    </Link>
-                </Container>
-                
+        return  (
+            <Container className="text-white">
+                <p className="h1">
+                    404.. This page is not found!
+                </p>
+                <p>
+                    So sorry about that!
+                </p>
+                <Link to="/">
+                    <Button>
+                        Return to homepage
+                    </Button>    
+                </Link>
+            </Container>
+        )
     }
 }
 
