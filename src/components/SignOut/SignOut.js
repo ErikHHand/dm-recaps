7ce4bec6284a6136e8aaa9aced2795282ab7eb90
Erik Handberg <erikhand@kth.se>
2020-04-16 15:58:57 +0200

    Fixes redirect when signing out

--- src/components/SignOut/SignOut.js
+++ src/components/SignOut/SignOut.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 import { withFirebase } from '../Firebase/Firebase';
 
 import {Button} from 'react-bootstrap';
 
 const SignOutButton = ({ firebase }) => (
-    <Button variant="secondary" onClick={firebase.doSignOut}>
-      	Sign Out
-    </Button>
+    <Link to="/signin">
+        <Button variant="secondary" onClick={firebase.doSignOut}>
+            Sign Out
+        </Button>          
+    </Link>
 );
 
 export default withFirebase(SignOutButton);
