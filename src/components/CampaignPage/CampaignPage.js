7bc079eb3670cb39a3e3d2b7aac2df85a4f509ea
Erik Handberg <erikhand@kth.se>
2020-04-21 22:24:52 +0200

    Minor style changes to campaign page to make it consistent with tag page and session page

--- src/components/CampaignPage/CampaignPage.js
+++ src/components/CampaignPage/CampaignPage.js
@@ -120,7 +120,7 @@ class CampaignPage extends Component {
 				
 				<Jumbotron fluid className="container">
 					<h1 className="center">Campaigns</h1>
-					<div className="campaign-list remove-scroll-bar">{campaigns}</div>
+					<div className="campaign-list remove-scroll-bar border-top border-bottom">{campaigns}</div>
 					<div className="center">
 						<Button variant="success" onClick={this.addCampaign}>Create a new campaign!</Button>
 					</div>
