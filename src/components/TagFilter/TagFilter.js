9d89451e2ee47dbf979d555081eaa2ea3787ba21
ErikHHand <48682153+ErikHHand@users.noreply.github.com>
2020-06-02 17:11:16 +0200

    Comments and clean up in the tag filter component

--- src/components/TagFilter/TagFilter.js
+++ src/components/TagFilter/TagFilter.js
@@ -15,7 +15,8 @@ import { withFirebase } from '../Firebase/Firebase';
 import * as firebase from 'firebase'; // Do not remove
 
 /*
-	This component holds the filter/search bar in the tag page
+	This component holds the filter/search bar in for tags. It is used both
+	on the tags page for the tag list, and in the tag selector pop-up.
 */
 class TagFilter extends Component {
 	constructor(props) {
@@ -32,21 +33,26 @@ class TagFilter extends Component {
 		this.filter = this.filter.bind(this);
 	}
 
+	// Create initial list of tags shown
 	componentDidMount() {
 		if(this.props.campaign.tags) {
 			this.filterKeys();
 		}
 	}
 
+	// Check if filter should be updated based on changes from other components
 	componentDidUpdate(prevProps) {
 
 		if (this.props.tagSort !== prevProps.tagSort) {
+			// Sorting order has changed
 			this.filterKeys();
 		} else if (this.state.numTags !== Object.keys(this.props.campaign.tags).length) {
+			// The number of tags has changed, from adding or deleting tags
 			this.filterKeys();	
 		}
 	}
-	  
+	
+	// Update text filter value and filter tags directly after
 	textFilter(value, event) {
 		event.preventDefault();
 		
@@ -55,22 +61,26 @@ class TagFilter extends Component {
 		}, this.filterKeys);
 	}
 
+	// Update type filter value and filter tags directly after
 	typeFilter(value) {	
 		this.setState({
 			typeFilter: value,
 		}, this.filterKeys);
 	}
 
+	// Filter the tags and return a list of filtered tag keys to
+	// the parent component
 	filterKeys() {
 		let keys = Object.keys(this.props.campaign.tags);
 
+		// Save number of tags in state for future comparisons
 		this.setState({
 			numTags: keys.length,
 		})
 
 		let sortedKeys;
 
-		// Filter results
+		// Filter results if there is a text filter present or type filter is not "All"
 		if(this.state.textFilter || this.state.typeFilter !== "All") {
 			keys = keys.filter((key, index, keys) => this.filter(key, index, keys));
 		}
@@ -91,6 +101,9 @@ class TagFilter extends Component {
 		this.props.handleFilteredTags(sortedKeys);
 	}
 
+	// The function used for filtering
+	// This function currently just uses the text filter to check for appearence in the beginning of the tag name.
+	// It uses the type filter to match with the tag type, if type filter valut is not "All"
 	filter(key, index, keys) {
 		let textFilter = this.props.campaign.tags[key].name.toLowerCase().startsWith(this.state.textFilter);
 		let typeFilter = this.state.typeFilter !== "All" ? this.props.campaign.tags[key].type === this.state.typeFilter : true;
@@ -99,6 +112,8 @@ class TagFilter extends Component {
 
 
 	render() {
+
+		// The dropdown list where a type for the filter is selected
 		let typeFilterItems = Object.keys(TYPES).map((type) => 
 			<Dropdown.Item key={type}>
 				<Badge 
@@ -113,14 +128,13 @@ class TagFilter extends Component {
 			</Dropdown.Item>
 		);
 
+		// The icon shown as the currently filtered type
 		let currentTypeFilter = "";
 		if(this.state.typeFilter === "All") {
 			currentTypeFilter = <FontAwesomeIcon icon={faFilter}/>
 		} else {
 			currentTypeFilter = <FontAwesomeIcon icon={ICONS[this.state.typeFilter]}/>
 		}
-		
-		
 
 		return (
 			<>
