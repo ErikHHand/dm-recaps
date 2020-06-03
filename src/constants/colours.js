a2b0879cc26656f002eb0a602aa8f98a81da9d8d
ErikHHand <48682153+ErikHHand@users.noreply.github.com>
2020-06-03 23:02:32 +0200

    Adds four more colours for tags

--- src/constants/colours.js
+++ src/constants/colours.js
@@ -1,4 +1,5 @@
 export const COLOURS = {
+	darkred: "#8B0000",
 	red: "#F1453D",
 	purple: "#9B2FAE",
 	indigo: "#4154B3",
@@ -6,14 +7,18 @@ export const COLOURS = {
 	cyan: "#1FBCD2",
 	teal: "#159688",
 	green: "#50AE55",
-	brown: "#785549",
 	lime: "#CCDA49",
+	thistle: "#D8BFD8",
 	yellow: "#FDE94E",
 	orange: "#FC9827",
+	black: "#000000",
 	grey: "#9E9E9E",
+	white: "#FFFFFF",
+	brown: "#785549",
 };
 
 export const TEXTCOLOURS = {
+	darkred: "text-white",
 	red: "text-white",
 	purple: "text-white",
 	indigo: "text-white",
@@ -22,13 +27,17 @@ export const TEXTCOLOURS = {
 	teal: "text-white",
 	green: "text-white",
 	brown: "text-white",
+	black: "text-white",
+	thistle: "text-black",
 	lime: "text-black",
 	yellow: "text-black",
 	orange: "text-black",
 	grey: "text-black",
+	white: "text-black",
 };
 
 export const COLOURSRGB = {
+	darkred: "rgba(139, 0, 0",
 	red: "rgba(241, 69, 61",
 	purple: "rgba(155, 47, 174",
 	indigo: "rgba(65, 84, 179",
@@ -37,8 +46,11 @@ export const COLOURSRGB = {
 	teal: "rgba(21, 150, 136",
 	green: "rgba(80, 174, 85",
 	brown: "rgba(120, 85, 73",
+	black: "rgba(0, 0, 0",
+	thistle: "rgba(216, 191, 216",
 	lime: "rgba(204, 218, 73",
 	yellow: "rgba(253, 233, 78",
 	orange: "rgba(252, 152, 39",
 	grey: "	rgba(158, 158, 158",
+	white: "rgba(255, 255, 255",
 };
